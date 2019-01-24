79c7d4d3af8284af47e3a7cc70e10e91e1dc62e6
Kevin Doan <1641539@brunel.ac.uk>
2019-01-24 15:25:12 +0000

    Initial commit

---
+++ app/src/main/java/com/example/cs17kkd/explorebrunel/Settings.java
@@ -0,0 +1,19 @@
+package com.example.cs17kkd.explorebrunel;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity {
+    private ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        actionBar=getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffd700")));
+    }
+}
---
+++ app/src/main/java/com/example/cs17kkd/explorebrunel/Template.java
@@ -0,0 +1,45 @@
+package com.example.cs17kkd.explorebrunel;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Locale;
+
+public class Template extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_template);
+
+        Button but = (Button) findViewById(R.id.button);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Categories =  new Intent(Template.this, com.example.cs17kkd.explorebrunel.Categories.class);
+                startActivity(Categories);
+
+            }
+        });
+        Button but2 = (Button) findViewById(R.id.button2);
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Favourites = new Intent(Template.this, com.example.cs17kkd.explorebrunel.Favourites.class);
+                startActivity(Favourites);
+
+            }
+        });
+        Button but3 = (Button) findViewById(R.id.button3);
+        but3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Settings = new Intent(Template.this, com.example.cs17kkd.explorebrunel.Settings.class);
+                startActivity(Settings);
+            }
+        });
+    }
+}
