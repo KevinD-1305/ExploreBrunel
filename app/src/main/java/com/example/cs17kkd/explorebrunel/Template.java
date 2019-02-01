1c458b03ed87faab558c5cf03b8847dca32a9803
KevinD-1305 <kev3001@hotmail.co.uk>
2019-02-01 10:50:05 +0000

    commit

--- app/src/main/java/com/example/cs17kkd/explorebrunel/Template.java
+++ app/src/main/java/com/example/cs17kkd/explorebrunel/Template.java
@@ -18,16 +18,14 @@ public class Template extends AppCompatActivity {
 
         Button but = (Button) findViewById(R.id.button);
         but.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
-                Intent Categories =  new Intent(Template.this, com.example.cs17kkd.explorebrunel.Categories.class);
+                Intent Categories = new Intent(Template.this, com.example.cs17kkd.explorebrunel.Categories.class);
                 startActivity(Categories);
 
             }
         });
         Button but2 = (Button) findViewById(R.id.button2);
         but2.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
                 Intent Favourites = new Intent(Template.this, com.example.cs17kkd.explorebrunel.Favourites.class);
                 startActivity(Favourites);
@@ -36,12 +34,18 @@ public class Template extends AppCompatActivity {
         });
         Button but3 = (Button) findViewById(R.id.button3);
         but3.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
                 Intent Settings = new Intent(Template.this, com.example.cs17kkd.explorebrunel.Settings.class);
                 startActivity(Settings);
             }
         });
-    }
+        Button but4 = (Button) findViewById(R.id.button4);
+        but4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent MainActivity = new Intent(Template.this, com.example.cs17kkd.explorebrunel.MainActivity.class);
+                startActivity(MainActivity);
+            }
+        });
 
+    }
 }
