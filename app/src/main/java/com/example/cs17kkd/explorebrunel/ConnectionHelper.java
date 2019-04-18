e77d91001d919b36414d965c1c1ca4efc70ea430
Talha Sabir <1518349@brunel.ac.uk>
2019-04-18 17:42:56 +0100

    Added ConnectionHelper class with connection string

---
+++ app/src/main/java/com/example/cs17kkd/explorebrunel/ConnectionHelper.java
@@ -0,0 +1,42 @@
+package com.example.cs17kkd.explorebrunel;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.test.suitebuilder.annotation.Suppress;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionHelper {
+    String IP, DB, DBUsername, DBPassword;
+
+    @SuppressLint("NewApi")
+    public Connection connections() {
+
+        IP = "";
+        DB = "";
+        DBUsername = "";
+        DBPassword = "";
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        java.sql.Connection connection = null;
+        String ConnectionURL = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://" + IP + ";databasename" + DB + ";user=" + DBUsername + ";password=" + DBPassword + ";";
+            connection = DriverManager.getConnection(ConnectionURL);
+        } catch (SQLException se) {
+            Log.e("error from sql", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("Error from Class", e.getMessage());
+        } catch (Exception ex) {
+            Log.e("Error from Exception", ex.getMessage());
+        }
+        return connection;
+
+
+    }
+}
