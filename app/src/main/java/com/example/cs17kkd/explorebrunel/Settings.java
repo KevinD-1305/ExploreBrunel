ac4ac35c3aee9e3fe6fe537f81d922117217c62a
Talha Sabir <1518349@brunel.ac.uk>
2019-04-18 23:58:44 +0100

    Added Test activity to get data from database

--- app/src/main/java/com/example/cs17kkd/explorebrunel/Settings.java
+++ app/src/main/java/com/example/cs17kkd/explorebrunel/Settings.java
@@ -1,5 +1,6 @@
 package com.example.cs17kkd.explorebrunel;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.Nullable;
@@ -9,10 +10,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
 
     private ActionBar actionBar;
+    Button settings_button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,14 @@ public class Settings extends AppCompatActivity {
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffd700")));
         getSupportActionBar().setDisplayShowHomeEnabled(true);//calls action bar
         getSupportActionBar().setIcon(R.drawable.settings);//shows the icon on action bar
+        settings_button1=(Button)findViewById(R.id.settings_btn1);
+
+        settings_button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),DBTestActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
