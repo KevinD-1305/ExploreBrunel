08ece85d684a5af7da141d727c1feb3718abdaa6
Talha Sabir <1518349@brunel.ac.uk>
2019-04-21 19:58:39 +0100

    activity displays selected data from database on textview widget

--- app/src/main/java/com/example/cs17kkd/explorebrunel/DBTestActivity2.java
+++ app/src/main/java/com/example/cs17kkd/explorebrunel/DBTestActivity2.java
@@ -3,6 +3,7 @@ package com.example.cs17kkd.explorebrunel;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 //import com.mysql.jdbc.ResultSetMetaData;
@@ -15,6 +16,9 @@ import java.sql.Statement;
 
 public class DBTestActivity2 extends AppCompatActivity {
 
+    TextView tv;
+    EditText et;
+
     private static final String url = "jdbc:mysql://192.168.1.149:3306/mytestdb";
     private static final String user = "talhasabir";
     private static final String pswd = "1234";
@@ -23,6 +27,7 @@ public class DBTestActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dbtest2);
+        testDB();
     }
 
     public void testDB(){
@@ -41,9 +46,9 @@ public class DBTestActivity2 extends AppCompatActivity {
             ResultSetMetaData rsmd = rs.getMetaData();
 
             while(rs.next()){
-                result += rsmd.getColumnName(1) + ": " + rs.getString(1) + "\n";
-                result += rsmd.getColumnName(2) + ": " + rs.getString(2) + "\n";
-                result += rsmd.getColumnName(3) + ": " + rs.getString(3) + "\n";
+                result += rs.getString(1) + "\n";
+                result += rs.getString(2) + "\n";
+                result += rs.getString(3) + "\n";
             }
             tv.setText(result);
 
