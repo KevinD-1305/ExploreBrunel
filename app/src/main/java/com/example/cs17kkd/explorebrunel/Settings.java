e0c6cc68753ede0d08206c6f0502c272cdfb7888
Lahaaz <33161719+Lahaaz@users.noreply.github.com>
2019-04-22 18:38:20 +0100

    colour page for the settings
    in this i have, created the colour button that takes the user to the colour page  within the settings page. however,it has non-functional buttons as of right now

--- app/src/main/java/com/example/cs17kkd/explorebrunel/Settings.java
+++ app/src/main/java/com/example/cs17kkd/explorebrunel/Settings.java
@@ -1,5 +1,6 @@
 package com.example.cs17kkd.explorebrunel;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.Nullable;
@@ -9,8 +10,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
+    private Button button1;
+
+
 
     private ActionBar actionBar;
 
@@ -22,5 +28,21 @@ public class Settings extends AppCompatActivity {
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffd700")));
         getSupportActionBar().setDisplayShowHomeEnabled(true);//calls action bar
         getSupportActionBar().setIcon(R.drawable.settings);//shows the icon on action bar
+
+       button1 = (Button) findViewById(R.id.button1);
+       button1.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               openActivity2();
+           }
+       });
     }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this,colours.class);
+        startActivity(intent);
+    }
+
 }
+
+
